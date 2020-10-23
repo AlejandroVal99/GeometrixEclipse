713727c6fa17a50a43201cef473eb1433c2fa541
AlejandroVal99 <joalevalverde@hotmail.com>
2020-10-23 12:53:48 -0500

    Shoot verification, drawWin, drawSelection, colors

--- src/view/spaceship/Triangletrix.java
+++ src/view/spaceship/Triangletrix.java
@@ -5,15 +5,20 @@ import processing.core.PImage;
 
 public class Triangletrix extends SpaceShip{
 
-	public Triangletrix(boolean player, String nickName, PImage inGame, PApplet app, int vida, PImage mybullet,PImage superShoot) {
-		super(player, nickName, inGame, app, vida, mybullet, superShoot);
+	public Triangletrix(boolean player, String nickName, PImage inGame, PImage inSelection, PApplet app, int vida,
+			PImage mybullet, PImage superShoot) {
+		super(player, nickName, inGame, inSelection, app, vida, mybullet, superShoot);
+		// TODO Auto-generated constructor stub
 		fireRate=1f;
 		superRate=4f;
 		dano=20;
 		danoSuper=50;
+		type = "tringletrix";
 	}
 
-	
+
+
+
 
 	
 
